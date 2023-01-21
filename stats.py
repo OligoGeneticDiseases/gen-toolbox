Removed mean normalisation code. Graphs now show raw enrichment ratio and the log2 of the value. Fixes graph title texts

stats.py
@@ -94,11 +94,11 @@ intersect_genes = ["AIP",
 rv_genes = ["BRCA1", "BRCA2", "CHEK2", "PALB2", "ATM"]
 # neg_control_genes = ["BLM", "CEBPA", "FANCA", "FANCB", "GATA2"]
 neg_control_genes = ["APC", "BMPR1A", "MSH2", "MSH6", "PTEN"]
-fraction_results = pd.DataFrame()
+
 fraction_results_2 = pd.DataFrame()
 
 
-def permutation_analysis(df_case, df_control, combination_length=5, iterations=1000):
+def permutation_analysis(df_case, df_control, combination_length=5, iterations=20000):
     """
     This permutation analysis is based on Monte Carlo analysis of permutations.
     Sets of N=5 random genes are selected from input variant frequency tables and variant enrichment is compared
@@ -128,7 +128,6 @@ def permutation_analysis(df_case, df_control, combination_length=5, iterations=1
                    len(df_case.columns)):  # Go through each frequency column, with 0 being the gene.col and simulate j times.
         j = 0
         frequency_column = df_case.columns[i]
-        vals = []
         vals2 = []
         while j < iterations:
             # indices = np.random.choice(df_fraction.shape[0], case_genes_length, replace=False)
@@ -140,41 +139,23 @@ def permutation_analysis(df_case, df_control, combination_length=5, iterations=1
             total_variants_control = df_control.loc[sampled_rows.index][frequency_column].sort_index()
             if total_variants_case.sum() != 0 and total_variants_control.sum() != 0:
                 # Difference of means
-                # Case 5 gene mean / control 5 gene mean (both mean normalized)
-
-                vals.append(np.divide(np.sum(total_variants_case - df_case_mean[frequency_column]),
-                                      np.sum(total_variants_control - df_control_mean[frequency_column])))
                 # Single sample average normalized, averages the variant enrichment level across all samples
                 # Depicts the size of the input dataframe (number of cases summed together)
                 # Currently a hardcoded value depending on the samples in the case group (1389) vs control group (826)
                 vals2.append(
                     np.divide(np.divide(total_variants_case.sum(), 1389), np.divide(total_variants_control.sum(), 826)))
             else:
-                vals.append(np.NaN)
                 vals2.append(np.NaN)
             j += 1
-        # sc.monte_carlo_test()
-        fraction_results[frequency_column] = vals
         fraction_results_2[frequency_column] = vals2
-    print(fraction_results)
+    print(fraction_results_2)
     # fraction_results["low.gnomad_5_100"] = np.log2(fraction_results["low.gnomad_5_100"].astype(dtype=float))
     # plt.hist(fraction_results["low.gnomad_5_100"], density=True, log=True, histtype="stepfilled", bins=100)
     # plt.hlines(data=fraction_results["low.gnomad_5_100_log"])
-    fig, axs = plt.subplots(len(fraction_results.columns), 2, sharex="none", tight_layout=True, figsize=(20, 24))
+    fig, axs = plt.subplots(len(fraction_results_2.columns), 2, sharex="none", tight_layout=True, figsize=(20, 24))
 
     i = 0
-    for frequency_column in fraction_results.columns:
-        # Mean normalization
-        # Calculate the enrichment ratio in the "likely impactful" gene group (q) vs.
-        # "likely non-impactful" genes (q_control)
-        q = np.sum(df_case[df_case.gene.isin(rv_genes)][frequency_column] - df_case_mean[frequency_column]) / np.sum(
-            df_control[df_control.gene.isin(rv_genes)][frequency_column] - df_control_mean[frequency_column])
-        q_control_group = np.sum(
-            df_case[df_case.gene.isin(neg_control_genes)][frequency_column] - df_case_mean[frequency_column]) / np.sum(
-            df_control[df_control.gene.isin(neg_control_genes)][frequency_column] - df_control_mean[frequency_column])
-        print("Impact group (mean-norm. ): {0}, case_genes_enrichment: {1}, control_genes_enrichment: {2}".format(
-            frequency_column,
-            q, q_control_group))
+    for frequency_column in fraction_results_2.columns:
 
         # Average sample normalization enrichment ratios for "likely impactful" and "likely non-impactful" genes
         q_avg = np.divide(np.sum(df_case[df_case.gene.isin(rv_genes)][frequency_column]) / 1389,
@@ -185,9 +166,8 @@ def permutation_analysis(df_case, df_control, combination_length=5, iterations=1
             frequency_column,
             q_avg, q_avg_control_group))
 
-        fraction_results[frequency_column + "_log2"] = np.log2(fraction_results[frequency_column].dropna())
         fraction_results_2[frequency_column + "_log2"] = np.log2(fraction_results_2[frequency_column].dropna())
-        if fraction_results[frequency_column].any():
+        if fraction_results_2[frequency_column].any():
             #mu, std = sp.norm.fit(fraction_results[frequency + "_log"].dropna())
             xmin, xmax = fraction_results_2[frequency_column + "_log2"].dropna().min(), fraction_results_2[
                 frequency_column + "_log2"].dropna().max()
@@ -198,23 +178,25 @@ def permutation_analysis(df_case, df_control, combination_length=5, iterations=1
 
             # Left sided plot for mean-normalized enrichment ratios
             axs[i, 0].hist(fraction_results_2[frequency_column + "_log2"], density=False,
-                           log=False, histtype="stepfilled", stacked=False, bins=400)
+                           log=False, histtype="stepfilled", stacked=False, bins=500)
             axs[i, 0].set_title(
                 '{0}: data_mean={1} case={2} control={3}'.format(
                     fraction_results_2[frequency_column].name + "_log2",
-                    np.round(fraction_results_2[frequency_column].mean(), 2),
-                    np.round(q_avg, 2), np.round(q_avg_control_group, 2)))
+                    np.round(fraction_results_2[frequency_column + "_log2"].mean(), 4),
+                    np.round(np.log2(q_avg), 2), np.round(np.log2(q_avg_control_group), 2)))
             axs[i, 0].axvline(np.log2(q_avg), color='orange')
             axs[i, 0].axvline(np.log2(q_avg_control_group), color="red")
 
             # Right-sided plots for avg-normalized enrichment ratios
-            axs[i, 1].hist(fraction_results_2[frequency_column + "_log2"], density=True,
-                           log=False, histtype="stepfilled", bins=400)
+            axs[i, 1].hist(fraction_results_2[frequency_column], density=False,
+                           log=False, histtype="stepfilled", bins=500)
             axs[i, 1].set_title(
-                "{0} data_mean {1}".format(fraction_results_2[frequency_column].name,
-                                           np.round(fraction_results_2[frequency_column].mean(), 2)))
-            axs[i, 1].axvline(np.log2(q_avg), color='orange')
-            axs[i, 1].axvline(np.log2(q_avg_control_group), color="red")
+                                '{0}: data_mean={1} case={2} control={3}'.format(
+                    fraction_results_2[frequency_column].name + "_log2",
+                    np.round(fraction_results_2[frequency_column].mean(), 2),
+                    np.round(q_avg, 2), np.round(q_avg_control_group, 2)))
+            axs[i, 1].axvline(q_avg, color='orange')
+            axs[i, 1].axvline(q_avg_control_group, color="red")
 
         i += 1
     fig.text(s=rv_genes, x=0.2, y=0.05, color="orange", ha="left")
@@ -248,9 +230,10 @@ if __name__ == "__main__":
     if args.out is None and len(outlines) > 0:
         filename = str(uuid.uuid4())
         with open(filename, "w+") as out:
-            out.write()
+            out.write(outlines)
         sys.stderr.write("Created output file {0}.".format(filename))
 
     permutation_analysis(rv_df, normal_df)
     end = datetime.datetime.now()
     print("Time: {0}".format(end - start))
+    exit()